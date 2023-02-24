f25aa3b8f224c32641804e3a7286d8a7bcce5bee
Yussif Muniru <yussifmunirium@gmail.com>
2023-02-24 21:53:07 +0000

    Working with website css

---
+++ monday/games/ob_lottery/3D_game.js
@@ -0,0 +1,445 @@
+// import the types of bets array
+import {playItemsName} from './rapid5D/data.js'
+
+
+// declare the buttons empty markup variable
+let buttonsMarkup = "";
+
+// declare the actions buttons empty markup variable
+let slotActionsButtonsMarkup = "";
+
+
+// declare the vertical actions buttons
+let verticalActionsButtonsAll = "";
+let verticalActionsButtonsClear = "";
+
+
+// declare the total number of buttons
+let allRowsArray = [];
+
+const slotsCommonClass = "ball-item";
+
+
+
+
+
+// This code executes once the DOM is ready
+$(function () {
+
+
+  console.log(playItemsName)
+
+  // prepare the entire slots markup
+  prepareSlots();
+
+  // call the checkUserBetState
+  buildVerticalButtons()
+
+  // build vertical buttons
+  // Loop through 10 slots to prepare the markup for each slot
+ 
+
+  $("div.ball").click(function () {
+    selectASlot(this);
+  });
+
+  $(".row-btns").click(function () {
+    handleRowButtons(this);
+  });
+
+
+});
+
+
+function buildVerticalButtons(){
+
+}
+
+
+function prepareSlots() {
+  let count = 0;
+  let rowNumber;
+
+  // Loop through 10 slots to prepare the markup for each slot
+  for (let index = 0; index < 60; index++) {
+    rowNumber = Math.trunc(index / 10);
+
+
+    
+    // Prepare the slots markup for each row
+    buttonsMarkup = buttonsMarkup.concat(
+      " ",
+      slots_buttons_markup(rowNumber, count)
+    )
+    
+
+
+    if (count < 6) {
+      // prepare the slots actions buttons markup
+      slotActionsButtonsMarkup = slotActionsButtonsMarkup.concat(
+        " ",
+        getSlotsActionsButtonsMarkup(rowNumber, count)
+      );
+    }
+
+    // increment the count
+    count += 1;
+
+    // for efficiency append the markup in multiples of 10
+    if (count == 10 && index < 50) {
+      buttonsMarkup = getListParentTag(
+        rowNumber,
+        buttonsMarkup.concat("", slotActionsButtonsMarkup)
+      );
+      updateListOfSlots();
+      buttonsMarkup = "";
+      slotActionsButtonsMarkup = "";
+      count = 0;
+    }
+    
+    if(index >= 50){
+
+       // build the all vertical buttons
+        verticalActionsButtonsAll = verticalActionsButtonsAll.concat(' ',getVerticalSlotsActionsButtonsMarkup(index,4))
+
+        verticalActionsButtonsClear = verticalActionsButtonsClear.concat(' ',getVerticalSlotsActionsButtonsMarkup(index,5,'Clear'))
+        
+    if(index === 59){
+            
+
+            $('#root-parent').append(getListParentTag(index , verticalActionsButtonsAll))
+            $('#root-parent').append(getListParentTag(index , verticalActionsButtonsClear))
+
+        }
+    
+
+    }
+   
+
+  }
+}
+
+
+// update the root element of the slots
+function updateListOfSlots() {
+  
+  $("#root-parent").append(buttonsMarkup);
+}
+
+/**
+ * Select a slot by adding highlight class to its span element and storing its digit in the appropriate row array.
+ * @param {object} slotObject - The DOM element of the selected slot.
+ */
+function selectASlot(slotObject) {
+  // Visually highlight the slot selection
+  const slotElement = $(slotObject).find(".4")[0];
+
+  $(slotElement).addClass("selected");
+
+  // Get the row number and selected slot digit
+  var rowNumber = getSlotsRowNumber(slotElement);
+  var selectedSlot = getSelectedSlotDigit(slotElement);
+
+  if (allRowsArray[rowNumber] !== undefined) {
+    if (allRowsArray[rowNumber].includes(selectedSlot)) {
+      console.log("deleting my code.......");
+      if ($(slotElement).hasClass("selected")) {
+        $(slotElement).removeClass("selected");
+      }
+
+      arrayRemove(allRowsArray[rowNumber], selectedSlot, true);
+    }
+  }
+
+  // add the bet slot selected to the appropriate array
+  allRowsArray[rowNumber] === undefined
+    ? (allRowsArray[rowNumber] = [selectedSlot])
+    : allRowsArray[rowNumber].push(selectedSlot);
+
+  // log all the selected bets
+  console.log(allRowsArray);
+}
+
+function arrayRemove(array, element, sort = false) {
+  array.splice(array.indexOf(element), 1);
+  sort && array.sort();
+}
+
+function getSlotsRowNumber(eventObject, slots = true) {
+  // Splits the class attribute of the event object into an array and selects the last element, which contains the row number.
+
+  console.log($(eventObject));
+
+  return slots
+    ? parseInt($(eventObject).attr("class").split(" ")[1])
+    : $(eventObject).attr("id").split("-");
+}
+
+/**
+ * Returns the selected slot digit from an event object's innerHTML.
+ * @param {Event} eventObject - The event object that was fired when the slot was selected.
+ * @returns {number} - The selected slot digit.
+ */
+function getSelectedSlotDigit(eventObject) {
+  return parseInt(eventObject.innerHTML);
+}
+
+
+
+function getSlotsActionsButtonsMarkup(rowNumber, count,) {
+  // Determines the ID for the button based on the slot index and row number.
+  let buttonId = `${slot_buttons_markup_text(count)}-${rowNumber}`;
+
+  // Determines the text to display on the button based on the button text and whether a secondary button exists.
+  let buttonTextDisplay = slot_buttons_markup_text(count);
+
+  return `<div class="row-btns ${rowNumber}" id='${buttonId}'> <span class="all">${buttonTextDisplay}</span></div>`;
+
+}
+
+
+
+
+function getVerticalSlotsActionsButtonsMarkup(index, count,displayText ='All') {
+    // Determines the ID for the button based on the slot index and row number.
+    let buttonId = `${slot_buttons_markup_text(count)}-${index}`;
+  
+    // Determines the text to display on the button based on the button text and whether a secondary button exists.
+    let style = index === 50 ? 'margin-left:0px;' : 'margin-left:13px;'
+  
+    return `<div style=${style} class="row-btns ${index}" id='${buttonId}'> <span class="all">${displayText}</span></div>`;
+  
+  }
+  
+
+
+/**
+ * Select all odd-numbered slots in the row of the clicked slot.
+ * Clears any previously selected slots.
+ *
+ * @param {Event} eventObject - The event object for the clicked slot.
+ */
+function handleRowButtons(eventObject,) {
+
+    // check the row number and the action performed
+    let rowNumberAndButtonType = getSlotsRowNumber(eventObject, false);
+
+    // get the action performed
+    let actionButtonType = rowNumberAndButtonType[0].trim();
+
+    // get the row number
+    let rowNumber = rowNumberAndButtonType[1];
+  
+    if(rowNumber >= 50){
+        console.log('entered here')
+        handleVerticalRowButtons(rowNumberAndButtonType)
+
+        return;
+    }
+    
+    // clear the row 
+    clearAllSelected(rowNumber);
+
+    // check to see if the action is to clear in the first place
+    if ("Clear" === actionButtonType) return;
+
+  // Get all the slots in the row of the clicked slot.
+  let rowSlots = $(document).find(`.${slotsCommonClass}.${rowNumber}`);
+  
+
+  // Loop through all the slots in the row and select the odd-numbered slots.
+  for (let index = 0; index < rowSlots.length; index++) {
+    
+    // get the digit of the slot
+    let selectedDigit = getSelectedSlotDigit(rowSlots[index]);
+
+    // check and decide the action to take
+    if (decideAction(selectedDigit,actionButtonType)) {
+
+
+      // remove it from the array if it is odd
+        allRowsArray[rowNumber] === undefined
+        ? (allRowsArray[rowNumber] = [selectedDigit])
+        : allRowsArray[rowNumber].push(selectedDigit);
+
+      // de-select it in the row
+      $(rowSlots[index]).addClass(`selected`);
+    }
+  }
+
+  // Log the array of first row selected slots to the console.
+  console.log(allRowsArray);
+}
+
+function handleVerticalRowButtons(actionAndRowNumber){
+
+    
+
+   
+    let actionButtonType = actionAndRowNumber[0]
+    let selectedDigit = actionAndRowNumber[1].split('').pop()
+     // Get all the slots in the row of the clicked slot.
+    let rowSlot;
+    console.log('here')
+    console.log(selectedDigit)
+
+
+    
+  
+  // Loop through all the slots in the row and select the odd-numbered slots.
+  for (let rowNumber = 0; rowNumber < 5; rowNumber++) {
+    
+   rowSlot = $(document).find(`#${rowNumber}-${selectedDigit}`)
+
+    // check and decide the action to take
+    if (actionButtonType === 'All') {
+
+         // remove it from the array if it is odd
+        allRowsArray[rowNumber] === undefined
+        ? (allRowsArray[rowNumber] = [selectedDigit])
+        : allRowsArray[rowNumber].push(selectedDigit);
+
+      // de-select it in the row
+      $(rowSlot).addClass(`selected`);
+
+
+    }else{
+
+        // remove it from the array if it is odd
+        ((allRowsArray[rowNumber] !== undefined)
+         && allRowsArray[rowNumber].includes(selectedDigit)) &&
+         arrayRemove(allRowsArray[rowNumber],selectedDigit)
+        
+ 
+       // de-select it in the row
+       $(rowSlot).removeClass(`selected`);
+
+    }
+  }
+
+
+}
+
+function clearAllSelected(rowNumber) {
+  // make sure atleast slot is selected
+  if (allRowsArray[rowNumber] === undefined) return;
+
+  // If there are no slots to clear, return and log a message.
+
+  if (allRowsArray[rowNumber].length === 0) {
+    console.log("Slots cleared for row: " + (rowNumber + 1));
+    return;
+  }
+
+  // clear all the slots of {rowNumber}
+  allRowsArray[rowNumber] = [];
+
+  // get all the selected slots
+  var allSlots = $(document).find(`.${slotsCommonClass}.${rowNumber}.selected`);
+
+  // de-select all selected slots
+  for (let index = 0; index < allSlots.length; index++) {
+    $(allSlots[index]).removeClass(`selected`);
+  }
+  console.log(allRowsArray);
+}
+
+// The following function takes a slot number and row number as inputs and returns a string representing the markup for the slot in the game table.
+function slots_buttons_markup(rowNumber, count) {
+  return `<div class="ball">
+          <div class="${slotsCommonClass} ${rowNumber}" id="${rowNumber}-${count}">${count}</div> 
+      <div class="ball-cm"></div>
+      </div>`;
+}
+
+function getListParentTag(row_number, children) {
+  return `<li class='balls-row'><div class='row-balls ${row_number}'>${children}</div></li>`;
+}
+
+function slot_buttons_markup_text(key) {
+  switch (key) {
+    case 0:
+      return "Odd";
+    case 1:
+      return "Even";
+    case 2:
+      return "Big";
+    case 3:
+      return "Small";
+    case 4:
+      return "All";
+    case 5:
+      return "Clear";
+    default:
+      return "Unknown Action";
+  }
+}
+
+
+function decideAction(selectedDigit,flag) {
+  
+
+    switch (flag.trim()) {
+      case "Odd":
+        return selectedDigit % 2 === 1;
+        
+      case "Even":
+        return selectedDigit % 2 === 0;
+       
+        case "Small":
+        return selectedDigit <= 0 || selectedDigit <= 4;
+      case "Big":
+        return selectedDigit >= 5 && selectedDigit <= 9;
+
+      case "All":
+        return true;
+     default:
+        console.log("Unknown action.");
+        break;
+    }
+
+
+  }
+
+
+  function betActions() {
+    
+    
+    let count = 0;
+    let rowNumber;
+  
+    // Loop through 10 slots to prepare the markup for each slot
+    for (let index = 0; index < 50; index++) {
+      rowNumber = Math.trunc(index / 10);
+  
+      // Prepare the slots markup for each row
+      buttonsMarkup = buttonsMarkup.concat(
+        " ",
+        slots_buttons_markup(rowNumber, count)
+      );
+  
+      if (count < 6) {
+        // prepare the slots actions buttons markup
+        slotActionsButtonsMarkup = slotActionsButtonsMarkup.concat(
+          " ",
+          getSlotsActionsButtonsMarkup(rowNumber, count)
+        );
+      }
+  
+      // increment the count
+      count += 1;
+  
+      // for efficiency append the markup in multiples of 10
+      if (count == 10) {
+        buttonsMarkup = getListParentTag(
+          rowNumber,
+          buttonsMarkup.concat("", slotActionsButtonsMarkup)
+        );
+        updateListOfSlots();
+        buttonsMarkup = "";
+        slotActionsButtonsMarkup = "";
+        count = 0;
+      }
+    }
+  }
+  
---
+++ monday/games/ob_lottery/rapid5D/data.js
@@ -0,0 +1,25 @@
+
+const playItemsName = {
+    'All-5':{
+        'Straight':{
+            'sub-name':['First 4','Last 4'],
+            'other':'',
+            0:'Joint',1:'Manual',2:'Combo'},
+        'Group 120':
+        {'Group 120':{'Prize':0,'htp':"How to play: Select 1 No. from 1st, 2nd, 3rd, 4th, 5th form 1bet. Selected and winning No. same & in order, as win. Win Sample：Bet plan: 1, 2, 3, 4, 5; Winning No.: 1, 2, 3, 4, 5, as win."},
+        1: 'Group 60',2: 'Group 30',3:'Group 20',4:'Group 10',5:'Group 5'}
+    },
+    'All-4':{
+        'sub-name':['First 4','Last 4'],
+        'other':'',
+        'Straight':{
+            0:'Joint',1:'Manual',2:'Combo'},
+        'Group':{
+            'sub-name':'First 4',
+            0: 'Group 120',1: 'Group 60',2: 'Group 30',3:'Group 20',4:'Group 10',5:'Group 5'}
+        
+    }
+}
+
+
+export {playItemsName}
